f88f8c098af60207c70ca9ebf218088e7aec6c0a
unknown <ankitpandey2708@gmail.com>
2025-05-05 16:54:49 +0530

    unused code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,22 +1,3 @@
-// Layout components
-export { default as ResponsiveContainer } from './ResponsiveContainer';
-export { default as GridSystem } from './GridSystem';
-export { default as BaselineGrid } from './BaselineGrid';
-export { default as ReadableText } from './ReadableText';
-export { default as ResponsiveText } from './ResponsiveText';
-export { default as TransitionWrapper } from './TransitionWrapper';
-export { default as AnimatedEntry } from './AnimatedEntry';
-export { default as ContentDivider } from './ContentDivider';
-export { default as HoverCard } from './HoverCard';
-
-// Image components
-export { default as ImageZoom } from './ImageZoom';
-export { default as DraggableImages } from './DraggableImages';
-export { default as MobileImageGallery } from './MobileImageGallery';
-export { default as ImageGalleryView } from './ImageGalleryView';
-export { default as DynamicGalleryView } from './DynamicGalleryView';
-export { default as ImageGridSkeleton } from './ImageGridSkeleton';
-
 // UI components
 export { Button } from './Button';
 export { default as CopyButton } from './CopyButton';
@@ -24,12 +5,7 @@ export { FolderSelector } from './FolderSelector';
 export { ProcessingIndicator } from './ProcessingIndicator';
 export { default as ProgressIndicator } from './ProgressIndicator';
 export { ErrorDisplay } from './ErrorDisplay';
-export { default as Breadcrumbs } from './Breadcrumbs';
 export { default as Skeleton, SkeletonText, SkeletonCard } from './Skeleton';
-
-// Accessibility components
-export { default as AccessibleLabel } from './AccessibleLabel';
-export { default as ScreenReaderText } from './ScreenReaderText';
 
 // Debug components
 export { DebugInfo } from './DebugInfo'; 
